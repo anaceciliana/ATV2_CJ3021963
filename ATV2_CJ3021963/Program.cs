d2113e57f2b0c27b5f1feb380e5867ad3d74eb62
CJ3021963 <CJ3021963@LAB01-PC01>
2024-02-28 09:07:00 -0300

    Adicionar arquivos de projeto.

---
+++ ATV2_CJ3021963/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ATV2_CJ3021963
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float salario;
+
+           //leitura salario
+           Console.WriteLine("Digite seu salário");
+            salario = float.Parse(Console.ReadLine());
+
+            salario = salario + (25.0f / 100.0f) * salario;
+
+            Console.WriteLine("O novo salário será de R$ {0}", salario);
+
+
+
+
+        }
+    }
+}
